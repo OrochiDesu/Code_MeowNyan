63af3ec76d8c2df67b62c95c2d1ed1035bac2e6b
Ashley Henry <ashley.henry@reedonline.co.uk>
2017-01-27 12:26:31 +0000

    initial commit

---
+++ Assets/MoveMe.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveMe : MonoBehaviour
+{
+    private Rigidbody _body;
+    private int _rotateSpeed = 5;
+    private int _force = 200;
+
+	void Start ()
+	{
+	    _body = GetComponent<Rigidbody>();
+	}
+
+	void Update ()
+	{
+	    var rot = 0;
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rot = 1;
+            _body.angularVelocity = Vector3.zero;
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rot = -1; 
+            _body.angularVelocity = Vector3.zero;
+        }
+
+	    transform.Rotate(new Vector3(0, 0, rot * _rotateSpeed));
+
+	    if (Input.GetKey(KeyCode.Space))
+	    {
+	        _body.AddForce(transform.up * _force);
+	    }
+
+	    if (Input.GetKeyDown(KeyCode.Return))
+	    {
+	        transform.position = Vector3.zero;
+            transform.rotation = Quaternion.identity;
+	        _body.velocity = new Vector3(0, 10, 0);
+            _body.angularVelocity = Vector3.zero;
+	    }
+	}
+}
---
+++ Assets/SpawnBall.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+public class SpawnBall : MonoBehaviour
+{
+    private Object _ballPrefab;
+    private float _ballSpawnInterval = 5;
+    private float _currentTime = 0;
+
+	// Use this for initialization
+	void Start ()
+    {
+        _ballPrefab = Resources.Load("Sphere");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    _currentTime += Time.deltaTime;
+
+	    if (_currentTime >= _ballSpawnInterval)
+	    {
+	        _currentTime = 0;
+	        var ball = (GameObject)Instantiate(_ballPrefab);
+            ball.transform.SetParent(transform);
+	        ball.transform.localPosition = Vector3.zero;
+
+            var rnd = new System.Random();
+
+	        var scale = rnd.Next(8);
+
+            ball.transform.localScale = new Vector3(scale, scale, scale);
+	    }
+	}
+}
